2276ccf6ee244fdcfde755381eb549328adc9fa7
bismark <bismark.john.anes@gmail.com>
2023-02-13 09:08:12 +0700

    use package @bismarkanes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Helmet } from 'react-helmet';
 import { useSelector, } from 'react-redux';
 import { appTitle, appDescription, } from 'config';
 import CustomRoute from 'components/customroute';
-import { ContainerBase } from 'container-base';
+import { ContainerBase } from '@bismarkanes/container-base';
 import Progress from 'components/progress';
 import { ExtraLarge, } from 'components/base/P';
 import Landing from 'containers/pages/landing';
