b90e321eb40937e4a3a4e2980f2b0bdf9614724d
bismark <bismark.john.anes@gmail.com>
2023-02-13 09:08:12 +0700

    use package @bismarkanes

--- src/containers/pages/landing/index.js
+++ src/containers/pages/landing/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ResponsiveGridContainer, ResponsiveGrid } from 'responsive-grid-container';
+import { ResponsiveGridContainer, ResponsiveGrid } from '@bismarkanes/responsive-grid-container';
 import { Normal } from 'components/base/P';
 import styled from 'styled-components';
 
@@ -16,12 +16,21 @@ const Landing = () => {
 
   return (
     <React.Fragment>
-      <ResponsiveGridContainer align="flex-start">
-        <ResponsiveSection>
+      <ResponsiveGridContainer justify="center">
+        <ResponsiveSection justify="center">
           <TextContainer>
-            1. Nullam eu ante vel est convallis dignissim.  Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio.  Nunc porta vulputate tellus.  Nunc rutrum turpis sed pede.  Sed bibendum.  Aliquam posuere.  Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.  Pellentesque condimentum, magna ut suscipit hendrerit, ipsum augue ornare nulla, non luctus diam neque sit amet urna.  Curabitur vulputate vestibulum lorem.  Fusce sagittis, libero non molestie mollis, magna orci ultrices dolor, at vulputate neque nulla lacinia eros.  Sed id ligula quis est convallis tempor.  Curabitur lacinia pulvinar nibh.  Nam a sapien.
+            TOP-Left
           </TextContainer>
         </ResponsiveSection>
+
+        <ResponsiveSection justify="center">
+          <TextContainer>
+            TOP-Right
+          </TextContainer>
+        </ResponsiveSection>
+      </ResponsiveGridContainer>
+
+      <ResponsiveGridContainer align="flex-start">
         <ResponsiveSection>
           <TextContainer>
             2. Nullam eu ante vel est convallis dignissim.  Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio.  Nunc porta vulputate tellus.  Nunc rutrum turpis sed pede.  Sed bibendum.  Aliquam posuere.  Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.  Pellentesque condimentum, magna ut suscipit hendrerit, ipsum augue ornare nulla, non luctus diam neque sit amet urna.  Curabitur vulputate vestibulum lorem.  Fusce sagittis, libero non molestie mollis, magna orci ultrices dolor, at vulputate neque nulla lacinia eros.  Sed id ligula quis est convallis tempor.  Curabitur lacinia pulvinar nibh.  Nam a sapien.
